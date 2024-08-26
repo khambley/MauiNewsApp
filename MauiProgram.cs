8d2aff3b3cb89af38b450910333b138d8ab1eb66
katherinehambley <superdesigngirl@outlook.com>
2024-08-26 08:28:21 -0500

    Added fontawesome for icons

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,15 +11,29 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("FontAwesome6Pro-Solid-900.otf", "FontAwesome");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .RegisterAppTypes();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
 	}
+    public static MauiAppBuilder RegisterAppTypes(this MauiAppBuilder mauiAppBuilder)
+    {
+        // ViewModels
+        mauiAppBuilder.Services.AddTransient<ViewModels.HeadlinesViewModel>();
+
+        // Views
+        mauiAppBuilder.Services.AddTransient<Views.AboutView>();
+        mauiAppBuilder.Services.AddTransient<Views.ArticleView>();
+        mauiAppBuilder.Services.AddTransient<Views.HeadlinesView>();
+
+        return mauiAppBuilder;
+    }
 }
 
