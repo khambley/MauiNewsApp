1accfd591f8a890b08e3e304fd9d44946ffcea9a
katherinehambley <superdesigngirl@outlook.com>
2024-08-26 10:46:21 -0500

    Wired up NewsService in HeadlinesViewModel

--- ViewModels/HeadlinesViewModel.cs
+++ ViewModels/HeadlinesViewModel.cs
@@ -1,11 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiNewsApp.Models;
+using MauiNewsApp.Services;
+
 namespace MauiNewsApp.ViewModels
 {
-	public class HeadlinesViewModel : BaseViewModel
+	public partial class HeadlinesViewModel : BaseViewModel
 	{
-		public HeadlinesViewModel()
+        private readonly INewsService newsService;
+
+        [ObservableProperty]
+        private NewsResult currentNews;
+
+        public HeadlinesViewModel(INewsService newsService)
 		{
+			this.newsService = newsService;
 		}
-	}
+
+		public async Task Initialize(string scope) => await Initialize(scope.ToLower() switch
+		{
+            "local" => NewsScope.Local,
+            "global" => NewsScope.Global,
+            "headlines" => NewsScope.Headlines,
+            _ => NewsScope.Headlines
+        });
+
+        public async Task Initialize(NewsScope scope)
+        {
+            CurrentNews = await newsService.GetNews(scope);
+        }
+
+        [RelayCommand]
+        public void ItemSelected(object selectedItem)
+        {
+            var selectedArticle = selectedItem as Article;
+            var url = HttpUtility.UrlEncode(selectedArticle.Url);
+            // Placeholder for more code later on
+        }
+    }
 }
 
