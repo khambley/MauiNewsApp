9d60244d90df1e380a5901ba45084253012a641f
katherinehambley <superdesigngirl@outlook.com>
2024-08-26 14:58:59 -0500

    Wired up HeadlinesVM to views

--- AppSettings.cs
+++ AppSettings.cs
@@ -4,7 +4,7 @@ namespace MauiNewsApp
 	internal static class AppSettings
 	{
         // TODO: Add API key when syncing changes to github
-        public static string NewsApiKey => "<Add API Key>";
+        public static string NewsApiKey => "c4223d51472942debe1d64e1e8cf5db2";
     }
 }
 
--- Views/HeadlinesView.xaml.cs
+++ Views/HeadlinesView.xaml.cs
@@ -1,9 +1,26 @@
-﻿namespace MauiNewsApp.Views;
+﻿using MauiNewsApp.ViewModels;
+
+namespace MauiNewsApp.Views;
 
 public partial class HeadlinesView : ContentPage
 {
-	public HeadlinesView()
+    readonly HeadlinesViewModel viewModel;
+
+    public HeadlinesView(HeadlinesViewModel viewModel)
 	{
-		InitializeComponent();
+        this.viewModel = viewModel;
+        InitializeComponent();
+        Task.Run(async () => await Initialize(GetScopeFromRoute()));
 	}
+    private async Task Initialize(string scope)
+    {
+        BindingContext = viewModel;
+        await viewModel.Initialize(scope);
+    }
+
+    private string GetScopeFromRoute()
+    {
+        var route = Shell.Current.CurrentState.Location.OriginalString.Split("/").LastOrDefault();
+        return route;
+    }
 }
